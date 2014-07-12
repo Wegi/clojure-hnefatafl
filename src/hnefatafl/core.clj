b72bec6e72e3a9e9587fc61f52f658789557bb60
Alexander Schneider <wegi.pwnz@googlemail.com>
2014-07-12 22:46:47 +0200

    add pve possibility and validity-checks to moves

--- src/hnefatafl/core.clj
+++ src/hnefatafl/core.clj
@@ -6,9 +6,6 @@
   [& args]
   (println "Hello, World!"))
 
-(def board
-  )
-
 (defn init-board
   "Initialize the board with the start configuration."
   []
@@ -23,3 +20,58 @@
    [:empty :empty :empty :empty :empty :empty :empty :empty :empty :empty :empty]
    [:empty :empty :empty :empty :empty :black :empty :empty :empty :empty :empty]
    [:castle :empty :empty :black :black :black :black :black :empty :empty :castle]])
+
+(defn field-free?
+  "Check if a given field is free"
+  [board coord]
+  (let [field (get-in board coord)]
+    (or (= field :empty)
+        (= field :throne)
+        (= field :castle))))
+
+(defn way-clear?
+  "Test if you can move a certain way."
+  [board [f-x f-y :as from] [t-x t-y :as to]]
+  (let [hor (map (fn [a] [a f-y]) (range (inc f-x) (inc t-x)))
+        vert (map (fn [a] [f-x a]) (range (inc f-y) (inc t-y)))
+        hormap (map (partial field-free? board) hor)
+        vertmap (map (partial field-free? board) vert)]
+    (not (or (some false? hormap) (some false? vertmap)))))
+
+(defn valid-move
+  "Check whether a move on a normal 11x11 Fetlar Hnefatafl is allowed"
+  [[from-x from-y :as from] [to-x to-y :as to] piece board]
+  (let [forbidden-set #{[0 0] [5 5] [0 10] [10 0] [10 10]}
+        destination (get-in board to)]
+    (if (or (or (< 10 from-x) (< 10 from-y) (< 10 to-x) (< 10 to-y))
+            (or (> 0 from-y) (> 0 from-x) (> 0 to-x) (> 0 to-y))
+            (and (not (= piece :king)) (contains? forbidden-set to))
+            (and (not (= from-x to-x)) (not (= from-y to-y)))
+            (not (field-free? board to))
+            (not (way-clear? board from to)))
+      false
+      true
+      )))
+
+(defn new-board
+  "Create a new board where one move is made."
+  [board from to]
+  (let [new-place (if (= from [5 5]) :throne :empty)]
+    (assoc-in (assoc-in board to :white) from new-place)))
+
+(defn move
+  "Move a figure on a given board. (move board player from-coord to-coord)"
+  [board player from-coord to-coord]
+  (let [piece (get-in board from-coord)]
+    (if (valid-move from-coord to-coord piece board)
+      (case player
+        :white-player (if (or (= piece :white) (= piece :king))
+                        (new-board board from-coord to-coord)
+                        :wrong-piece)
+        :black-player (if (= piece :black)
+                        (new-board board from-coord to-coord)
+                        :wrong-piece)
+        :not-a-player)
+      :move-forbidden)))
+
+
--- test/hnefatafl/core_test.clj
+++ test/hnefatafl/core_test.clj
@@ -8,3 +8,13 @@
   (((init-board) 5) 6) => :white
   (((init-board) 0) 0) => :castle
   (((init-board) 0) 10) => :castle)
+
+(fact "Test the move mechanism"
+  (get-in (move (init-board) :white-player [5 3] [9 3]) [9 3]) => :white
+  (move (init-board) :white-player [5 1] [9 1]) => :wrong-piece
+  (move (init-board) :black-player [5 1] [9 2]) => :move-forbidden
+  (move (init-board) :shrek [5 1] [9 1]) => :not-a-player
+  (get-in (move (init-board) :white-player [5 3] [9 3]) [5 3]) => :empty
+  (move (init-board) :white-player [5 5] [8 5]) => :move-forbidden
+  (move (init-board) :black-player [5 1] [11 1]) => :move-forbidden)
+
